2861acf0a079ec083e699a698e9566a69c2bb7b0
Iñaki Quesada <iquesada@piedresybarro.com>
2019-05-06 20:27:11 +0200

    Issue fixed

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,10 +9,13 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :eurovision2019, Eurovision2019Web.Endpoint,
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  live_view: [
+    signing_salt: System.get_env("LIVE_VIEW_SALT")
+  ]
 
 # Configure your database
 config :eurovision2019, Eurovision2019.Repo,
-  ssl: true
+  ssl: true,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
