114db1e24a7a7a261269d6bb064412b7b675af4d
Iñaki Quesada <iquesada@piedresybarro.com>
2019-05-06 22:18:08 +0200

    Hostname added

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -9,6 +9,10 @@ use Mix.Config
 # kept out of version control and might be hard to recover
 # or recreate for your teammates (or yourself later on).
 config :eurovision2019, Eurovision2019Web.Endpoint,
+  http: [:inet6, port: System.get_env("PORT") || 4000],
+  url: [scheme: "https", host: "shielded-savannah-49393", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   live_view: [
     signing_salt: System.get_env("LIVE_VIEW_SALT")
